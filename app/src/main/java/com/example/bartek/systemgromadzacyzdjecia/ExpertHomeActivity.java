b2893c4d38483577ab6869f11558fc8792f8132d
Kuba Kaczmarek <kubak3marek@gmail.com>
2018-08-06 12:23:25 +0200

    Add the LogOut button to expert home screen

--- app/src/main/java/com/example/bartek/systemgromadzacyzdjecia/ExpertHomeActivity.java
+++ app/src/main/java/com/example/bartek/systemgromadzacyzdjecia/ExpertHomeActivity.java
@@ -1,10 +1,14 @@
 package com.example.bartek.systemgromadzacyzdjecia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,12 +16,17 @@ import java.util.List;
 public class ExpertHomeActivity extends AppCompatActivity implements View.OnClickListener
 {
     private List<String> list;
+    private FirebaseAuth firebaseAuth;
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expert_home);
+        firebaseAuth = FirebaseAuth.getInstance();
+        logoutButton = findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(this);
         PrepareTexts();
         initRecyclerView();
     }
@@ -41,6 +50,13 @@ public class ExpertHomeActivity extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View v) {
-
+        {
+            if (v == logoutButton)
+            {
+                firebaseAuth.signOut();
+                finish();
+                startActivity(new Intent(this,LoginActivity.class));
+            }
+        }
     }
 }
